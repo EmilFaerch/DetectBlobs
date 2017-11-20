4e105b1539e6c58894d82cbe157fd3b54ef5996a
Unknown <emil.faerch@live.dk>
2017-11-20 16:06:02 +0100

    Moving away from templateMatch and into Contours/BLOBs

--- DetectBlobs/main.cpp
+++ DetectBlobs/main.cpp
@@ -18,9 +18,10 @@ void showOutputLoop();
 void findObject(Mat objTemplate, string name, double rotation, int team);
 void findAllOnce();
 
+
 /* References */
 Mat input, background, output;
-
+/* No more template matching ------------
 Mat p1_ship = imread("p1_ship.PNG", 0); 
 Mat p1_sq1 = imread("p1_sq1.PNG", 0);
 Mat p1_sq2 = imread("p1_sq2.PNG", 0);
@@ -32,17 +33,39 @@ Mat p2_sq2 = imread("p2_sq2.PNG", 0);
 double team1_minMatchValue = 0.75;
 double team2_minMatchValue = 0.7;
 double bestMatch = 0;
+--------------------------------------- */
 
-recursive_mutex mtx, mtxloop;
+double p1_shipA, p1_sq1A, p1_sq2a;
+double p2_shipA, p2_sq1A, p2_sq2a;
+
+vector<vector<Point> > contours;
+vector<Vec4i> hierarchy;
 
 int main(int, char)
 {
-	Sleep(2000);
-
-	takeBackground();
-
+	input = imread("input image rotated.PNG", 0);
+	output = Mat::zeros(input.rows, input.cols, CV_8UC1);
 	thread threadOutput(showOutputLoop);
 
+	// -v- Uncomment -----^ testing purposes
+	//Sleep(2000);
+	//takeBackground();
+	//thread threadOutput(showOutputLoop);
+
+	/// Find contours
+	findContours(input, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+	vector<Rect> boundRect(contours.size());
+
+	for (int i = 0; i < contours.size(); i++){
+		boundRect[i] = boundingRect(Mat(contours[i]));
+		drawContours(output, contours, i, Scalar::all(255));
+		rectangle(output, boundRect[i].tl(), boundRect[i].br(), Scalar::all(100), 2, 8, 0);
+
+		double area = contourArea(contours[i]);
+		cout << "x, y: " << boundRect[i].x + (boundRect[i].width / 2) << ", " << boundRect[i].y + (boundRect[i].height / 2) << " - Area: " << area << endl;
+	}
+
+/*
 	cout << "Looking for P1 Ship" << endl;
 	thread p1_ship(findObject, p1_ship, "P1 Ship", 0, 1);
 	cout << "Looking for P1 SQ1" << endl;
@@ -56,11 +79,11 @@ int main(int, char)
 	thread p2_sq1(findObject, p2_sq1, "P2 SQ1", 0, 1);
 	cout << "Looking for P2 SQ2" << endl;
 	thread p2_sq2(findObject, p2_sq2, "P2 SQ2", 0, 2);
-
-//	findAllOnce();
+*/
 
 	waitKey(0);
 
+/*
 	p1_ship.join();
 	p1_sq1.join();
 	p1_sq2.join();
@@ -68,6 +91,7 @@ int main(int, char)
 	p2_ship.join();
 	p2_sq1.join();
 	p2_sq2.join();
+	*/
 
 	threadOutput.join();
 
@@ -84,35 +108,32 @@ void takeBackground(){
 	input = desktopCapture(input);
 	cvtColor(input, input, CV_RGB2GRAY);
 
-//	increaseContrast(background, 10);
-//	GaussianBlur(background, background, Size(3, 3), 1.5, 1.5);
-//	medianBlur(background, background, 5);
-
-//	increaseContrast(input, 10);
-//	GaussianBlur(input, input, Size(3, 3), 1.5, 1.5);
+	GaussianBlur(background, background, Size(3, 3), 1.5, 1.5);
+	GaussianBlur(input, input, Size(3, 3), 1.5, 1.5);
 //	medianBlur(input, input, 5);
 
 	input = input - background; 
 
-//	increaseContrast(input, 10);
-//	GaussianBlur(input, input, Size(3, 3), 1.5, 1.5);
+	erode(input, input, Mat());
+	dilate(input, input, Mat());
 
-//	makeBinary(input, 17);
 	threshold(input, input, 15, 255, THRESH_OTSU);
 
-	GaussianBlur(p1_ship, p1_ship, Size(3, 3), 1.5, 1.5); GaussianBlur(p2_ship, p2_ship, Size(3, 3), 1.5, 1.5);
-	GaussianBlur(p1_sq1, p1_sq1, Size(3, 3), 1.5, 1.5); GaussianBlur(p2_sq1, p2_sq1, Size(3, 3), 1.5, 1.5);
-	GaussianBlur(p1_sq2, p1_sq2, Size(3, 3), 1.5, 1.5); GaussianBlur(p2_sq2, p2_sq2, Size(3, 3), 1.5, 1.5);
+//	imwrite("input image rotated.png", input);
+
+	//GaussianBlur(p1_ship, p1_ship, Size(3, 3), 1.5, 1.5); GaussianBlur(p2_ship, p2_ship, Size(3, 3), 1.5, 1.5);
+	//GaussianBlur(p1_sq1, p1_sq1, Size(3, 3), 1.5, 1.5); GaussianBlur(p2_sq1, p2_sq1, Size(3, 3), 1.5, 1.5);
+	//GaussianBlur(p1_sq2, p1_sq2, Size(3, 3), 1.5, 1.5); GaussianBlur(p2_sq2, p2_sq2, Size(3, 3), 1.5, 1.5);
 
 	input.copyTo(output);
 }
 
-
+// For template matching; not used anymore
+/*
 void findObject(Mat objTemplate, string name, double rotation, int team){
 	double minMatchValue = team1_minMatchValue;
 		if (team == 2) minMatchValue = team2_minMatchValue;
 
-//	mtx.lock();
 	Mat result;
 	Mat temp; objTemplate.copyTo(temp);
 
@@ -122,7 +143,7 @@ void findObject(Mat objTemplate, string name, double rotation, int team){
 		warpAffine(temp, temp, rot, temp.size());
 	}
 
-	matchTemplate(input, temp, result, CV_TM_CCORR_NORMED);
+	matchTemplate(input, temp, result, 5);
 
 	double minVal; double maxVal; Point minLoc; Point maxLoc; Point matchLoc;
 
@@ -130,15 +151,12 @@ void findObject(Mat objTemplate, string name, double rotation, int team){
 
 	matchLoc = maxLoc;
 
-//	if (maxVal > bestMatch) bestMatch = maxVal;
-
 	if (maxVal > minMatchValue){
 
 		rectangle(output, matchLoc, Point(matchLoc.x + temp.cols, matchLoc.y + temp.rows), Scalar::all(255), 2, 8, 0);
 		putText(output, name, Point(matchLoc.x + temp.cols + 5, matchLoc.y + temp.rows - 5), 1, 1, Scalar(255, 0, 0, 255));
 
 		cout << name + " (" + to_string(maxVal) + " percent match) found at: " + to_string(matchLoc.x) + ", " + to_string(matchLoc.y) << " - rotation: " << rotation << endl;
-//		findObject(objTemplate, name, 0, team);
 	}
 	else
 	{
@@ -150,11 +168,11 @@ void findObject(Mat objTemplate, string name, double rotation, int team){
 		}
 		else
 		{
-			cout << "Did not find a match for " + name + " - best match: " << bestMatch << endl;
+			cout << "Did not find a match for " + name + "."<< endl;
 		}
 	}
-//	mtx.unlock();
 }
+
 
 void findAllOnce(){
 	bestMatch = 0;
@@ -171,33 +189,12 @@ void findAllOnce(){
 	bestMatch = 0;
 	findObject(p2_sq2, "P2 SQ2", 0, 2);
 }
+*/
 
 void showOutputLoop(){
 	while (true){
 		imshow("Output", output);
 		if (waitKey(30) > 0) break;
-	}
-}
-
-void makeBinary(Mat temp, int threshold){
-	for (int y = 0; y < temp.rows; y++){
-		for (int x = 0; x < temp.cols; x++){
-			if (temp.at<uchar>(y, x) <= threshold) temp.at<uchar>(y, x) = 0;
-			else temp.at<uchar>(y, x) = 255;
-		}
-	}
-
-	erode(temp, temp, Mat());
-	dilate(temp, temp, Mat());
-}
-
-void increaseContrast(Mat temp, int amount){
-	for (int y = 0; y < temp.rows; y++){
-		for (int x = 0; x < temp.cols; x++)
-		{
-			if (temp.at<uchar>(y, x) * amount > 255) temp.at<uchar>(y, x) = 255;
-			else temp.at<uchar>(y, x) = temp.at<uchar>(y, x) * amount;
-		}
 	}
 }
 
