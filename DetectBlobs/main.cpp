36f6ebff50bf481989f4e238f6b4632377c8b62e
EmilFaerch <emil.faerch@live.dk>
2017-11-20 11:02:57 +0100

    Total update of

--- DetectBlobs/main.cpp
+++ DetectBlobs/main.cpp
@@ -2,70 +2,123 @@
 #include <Windows.h>
 #include <iostream><
 #include <thread>
+#include <mutex>
 
 using namespace cv;
 using namespace std;
 
 /* Function Headers */
+// Initializing and Processing ----------------------
 Mat desktopCapture(Mat input);
-void increaseContrast(Mat input, int amount);
-void findObject(Mat objTemplate, string name, double rotation);
-void findAll();
+void takeBackground();
+void increaseContrast(Mat temp, int amount);
+void makeBinary(Mat temp, int threshold);
+void showOutputLoop();
+// Find objects (Ships/Squadrons) -------------
+void findObject(Mat objTemplate, string name, double rotation, int team);
+void findAllOnce();
 
 /* References */
-Mat input, output;
-Mat result = input.clone();
+Mat input, background, output;
 
-// Sorte Figurer
-Mat p1_ship = imread("p1_ship.PNG", 0); // Kors
-Mat p1_sq1 = imread("p1_sq1.PNG", 0);	// Ruder
-Mat p1_sq2 = imread("p1_sq2.PNG", 0);	// Cirkel
+Mat p1_ship = imread("p1_ship.PNG", 0); 
+Mat p1_sq1 = imread("p1_sq1.PNG", 0);
+Mat p1_sq2 = imread("p1_sq2.PNG", 0);
 
-// Hvide Figurer
-Mat p2_ship = imread("p2_ship.PNG", 0);	// Kors
-Mat p2_sq1 = imread("p2_sq1.PNG", 0);	// Ruder
-Mat p2_sq2 = imread("p2_sq2.PNG", 0);	// Cirkel
+Mat p2_ship = imread("p2_ship.PNG", 0);
+Mat p2_sq1 = imread("p2_sq1.PNG", 0);
+Mat p2_sq2 = imread("p2_sq2.PNG", 0);
 
-double minMatchValue = 0.70;
+double team1_minMatchValue = 0.75;
+double team2_minMatchValue = 0.62;
 double bestMatch = 0;
+
+recursive_mutex mtx, mtxloop;
 
 int main(int, char)
 {
 	Sleep(2000);
 
-	input = desktopCapture(input);
-	cvtColor(input, input, CV_RGB2GRAY);
+	takeBackground();
 
-	increaseContrast(input, 4);
-	increaseContrast(p1_ship, 4); increaseContrast(p1_sq1, 4); increaseContrast(p1_sq2, 4);
-	increaseContrast(p2_ship, 4); increaseContrast(p2_sq1, 4); increaseContrast(p2_sq2, 4);
+	thread threadOutput(showOutputLoop);
 
-	output = input.clone();
+	cout << "Looking for P1 Ship" << endl;
+	thread p1_ship(findObject, p1_ship, "P1 Ship", 0, 1);
+	cout << "Looking for P1 SQ1" << endl;
+	thread p1_sq1(findObject, p1_sq1, "P1 SQ1", 0, 1);
+	cout << "Looking for P1 SQ2" << endl;
+	thread p1_sq2(findObject, p1_sq2, "P2 SQ2", 0, 2);
 
-	findAll();
+	cout << "Looking for P2 Ship" << endl;
+	thread p2_ship(findObject, p2_ship, "P2 Ship", 0, 1);
+	cout << "Looking for P2 SQ1" << endl;
+	thread p2_sq1(findObject, p2_sq1, "P2 SQ1", 0, 1);
+	cout << "Looking for P2 SQ2" << endl;
+	thread p2_sq2(findObject, p2_sq2, "P2 SQ2", 0, 2);
+
+//	findAllOnce();
 
 	waitKey(0);
+
+	p1_ship.join();
+	p1_sq1.join();
+	p1_sq2.join();
+
+	p2_ship.join();
+	p2_sq1.join();
+	p2_sq2.join();
+
+	threadOutput.join();
 
 	return 0;
 }
 
-void findObject(Mat objTemplate, string name, double rotation){
-	Mat temp;
-	objTemplate.copyTo(temp);
-	int background = temp.at<uchar>(0, 0);
+void takeBackground(){
+	background = desktopCapture(background);
+	cvtColor(background, background, CV_RGB2GRAY);
+
+	imshow("[BG] Press Space to continue", background);
+	waitKey(0);
+
+	input = desktopCapture(input);
+	cvtColor(input, input, CV_RGB2GRAY);
+
+//	increaseContrast(background, 10);
+	GaussianBlur(background, background, Size(3, 3), 1.5, 1.5);
+	medianBlur(background, background, 5);
+
+//	increaseContrast(input, 10);
+	GaussianBlur(input, input, Size(3, 3), 1.5, 1.5);
+	medianBlur(input, input, 5);
+
+	input = input - background; 
+
+	increaseContrast(input, 10);
+	GaussianBlur(input, input, Size(3, 3), 1.5, 1.5);
+
+	makeBinary(input, 17);
+
+	GaussianBlur(p1_ship, p1_ship, Size(3, 3), 1.5, 1.5); GaussianBlur(p2_ship, p2_ship, Size(3, 3), 1.5, 1.5);
+	GaussianBlur(p1_sq1, p1_sq1, Size(3, 3), 1.5, 1.5); GaussianBlur(p2_sq1, p2_sq1, Size(3, 3), 1.5, 1.5);
+	GaussianBlur(p1_sq2, p1_sq2, Size(3, 3), 1.5, 1.5); GaussianBlur(p2_sq2, p2_sq2, Size(3, 3), 1.5, 1.5);
+
+	input.copyTo(output);
+}
+
+
+void findObject(Mat objTemplate, string name, double rotation, int team){
+	double minMatchValue = team1_minMatchValue;
+		if (team == 2) minMatchValue = team2_minMatchValue;
+
+//	mtx.lock();
+	Mat result;
+	Mat temp; objTemplate.copyTo(temp);
 
 	if (rotation != 0){
 		Point2f tempCenter(temp.cols / 2.0, temp.rows / 2.0);
 		Mat rot = cv::getRotationMatrix2D(tempCenter, rotation, 0.9);
 		warpAffine(temp, temp, rot, temp.size());
-		
-		for (int y = 0; y < temp.rows; y++){
-			for (int x = 0; x < temp.cols; x++){
-				if (temp.at<uchar>(y, x) == 0){
-					temp.at<uchar>(y, x) = background;
-				}
-			}
-		}
 	}
 
 	matchTemplate(input, temp, result, 5);
@@ -76,7 +129,7 @@ void findObject(Mat objTemplate, string name, double rotation){
 
 	matchLoc = maxLoc;
 
-	if (maxVal > bestMatch) bestMatch = maxVal;
+//	if (maxVal > bestMatch) bestMatch = maxVal;
 
 	if (maxVal > minMatchValue){
 
@@ -84,55 +137,73 @@ void findObject(Mat objTemplate, string name, double rotation){
 		putText(output, name, Point(matchLoc.x + temp.cols + 5, matchLoc.y + temp.rows - 5), 1, 1, Scalar(255, 0, 0, 255));
 
 		cout << name + " (" + to_string(maxVal) + " percent match) found at: " + to_string(matchLoc.x) + ", " + to_string(matchLoc.y) << " - rotation: " << rotation << endl;
+//		findObject(objTemplate, name, 0, team);
 	}
-	else 
+	else
 	{
-//		cout << "Not found where Rotation = " << rotation << " - Best match: " << maxVal << " - temp.size: " << temp.size() << endl;
+		//	cout << "Not found where Rotation = " << rotation << " - Best match: " << maxVal << " - temp.size: " << temp.size() << endl;
 
 		if (rotation < 360){
 			rotation += 1;
-			findObject(objTemplate, name, rotation);
+			findObject(objTemplate, name, rotation, team);
 		}
 		else
 		{
 			cout << "Did not find a match for " + name + " - best match: " << bestMatch << endl;
 		}
 	}
-
-	imshow("Output", output);
-	imshow("Template", temp);
-
-//	waitKey(0);
+//	mtx.unlock();
 }
 
-void findAll(){
+void findAllOnce(){
 	bestMatch = 0;
-	findObject(p1_ship, "P1 Ship (Sort Rude)", 0);
+	findObject(p1_ship, "P1 Ship", 0, 1);
 	bestMatch = 0;
-	findObject(p1_sq1, "P1 SQ1 (Hvid Rude)", 0);
+	findObject(p1_sq1, "P1 SQ1", 0, 1);
 	bestMatch = 0;
-	findObject(p1_sq2, "P1 SQ2 (Hvid O)", 0);
+	findObject(p1_sq2, "P1 SQ2", 0, 1);
 
-	//bestMatch = 0;
-	//findObject(p2_ship, "P2 Ship (asd Kors)", 0);
-	//bestMatch = 0;
-	//findObject(p2_sq1, "P2 SQ1 (Hvid Ruder)", 0);
-	//bestMatch = 0;
-	//findObject(p2_sq2, "P2 SQ2 (Hvidt O)", 0);
+	bestMatch = 0;
+	findObject(p2_ship, "P2 Ship", 0, 2);
+	bestMatch = 0;
+	findObject(p2_sq1, "P2 SQ1", 0, 2);
+	bestMatch = 0;
+	findObject(p2_sq2, "P2 SQ2", 0, 2);
 }
 
-void increaseContrast(Mat input, int amount){
-	for (int y = 0; y < input.rows; y++){
-		for (int x = 0; x < input.cols; x++){
-			if (!(input.at<uchar>(y, x) * amount > 255)) input.at<uchar>(y, x) = input.at<uchar>(y, x) * amount;
+void showOutputLoop(){
+	while (true){
+		imshow("Output", output);
+		if (waitKey(30) > 0) break;
+	}
+}
+
+void makeBinary(Mat temp, int threshold){
+	for (int y = 0; y < temp.rows; y++){
+		for (int x = 0; x < temp.cols; x++){
+			if (temp.at<uchar>(y, x) <= threshold) temp.at<uchar>(y, x) = 0;
+			else temp.at<uchar>(y, x) = 255;
+		}
+	}
+
+	erode(temp, temp, Mat());
+	dilate(temp, temp, Mat());
+}
+
+void increaseContrast(Mat temp, int amount){
+	for (int y = 0; y < temp.rows; y++){
+		for (int x = 0; x < temp.cols; x++)
+		{
+			if (temp.at<uchar>(y, x) * amount > 255) temp.at<uchar>(y, x) = 255;
+			else temp.at<uchar>(y, x) = temp.at<uchar>(y, x) * amount;
 		}
 	}
 }
 
-// Found this snippet online -- make sure you understand it, censor might ask us wtf it is. 
-// We don't have to be able to explain everything, just what it does.
 Mat desktopCapture(Mat input)
 {
+	// Found this snippet online -- make sure you understand it, censor might ask us wtf it is. 
+	// We don't have to be able to explain everything, just what it does.
 	HWND desktop = GetDesktopWindow();
 	HDC hwindowDC, hwindowCompatibleDC;
 
